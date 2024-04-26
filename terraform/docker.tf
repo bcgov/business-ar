84571c2f879251636616946a64fdd5b905d8cfbd
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-04-26 13:32:25 -0700

    pass at config

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -10,6 +10,12 @@ terraform {
 provider docker {
 }
 
+resource "random_string" "app_secret" {
+  length           = 16
+  special          = false
+  override_special = "/@\" "
+}
+
 resource docker_image "regapi_image" {
   name = "regapi"
 }
@@ -21,6 +27,54 @@ resource docker_container "regapi_container" {
     internal = 8080
     external = 8080
   }
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
+  env = [ 
+    "FLASK_ENV=development",
+    "FLASK_APP=wsgi.py",
+    "SECRET_KEY=${md5(random_string.app_secret.result)}",
+    "APP_SETTINGS=dev",
+
+    "DATABASE_USERNAME=padmin",
+    "DATABASE_PASSWORD=${random_string.postgresAppPassword.result}",
+    "DATABASE_NAME=business-ar",
+    "DATABASE_HOST=reg_postgres",
+    "DATABASE_PORT=5432",
+
+    "DATABASE_TEST_USERNAME=padmin",
+    "DATABASE_TEST_PASSWORD=${random_string.postgresAppPassword.result}",
+    "DATABASE_TEST_NAME=business-ar-test",
+    "DATABASE_TEST_HOST=reg_postgres",
+    "DATABASE_TEST_PORT=5432",
+
+    "AUTH_API_URL=${var.auth_api_url}",
+    "PAY_API_URL=${var.pay_api_url}",
+
+    "JWT_OIDC_ALGORITHMS=RS256",
+    "JWT_OIDC_AUDIENCE=account-services",
+    "JWT_OIDC_CACHING_ENABLED=True",
+    "JWT_OIDC_CLIENT_SECRET=${random_string.app_secret.result}",
+    "JWT_OIDC_ISSUER=${var.oidc_issuer}",
+    "JWT_OIDC_JWKS_CACHE_TIMEOUT=6000",
+    "JWT_OIDC_WELL_KNOWN_CONFIG=${var.oidc_well_known_config}",
+
+    "AUTH_SVC_URL=${var.auth_svc_url}",
+    "AUTH_SVC_CLIENT_ID=${var.auth_client_id}",
+    "AUTH_SVC_CLIENT_SECRET=${var.auth_client_secret}",
+
+  ]
+}
+
+data "template_file" "firebase_adminsdk_json" {
+  template = file("${path.module}/project-firebase-adminsdk.json")
+  vars = {
+  }
+}
+
+resource "local_file" "postgres_script" {
+  content  = data.template_file.firebase_adminsdk_json.rendered
+  filename = "${var.hostRootPath}/data/web/project-firebase-adminsdk.json"
 }
 
 resource docker_image "regweb_image" {
@@ -34,6 +88,29 @@ resource docker_container "regweb_container" {
     internal = 3000
     external = 3000
   }
+  ports {
+    internal = 4000
+    external = 4000
+  }
+
+  volumes {
+    host_path      = "${var.hostRootPath}/data/web"
+    container_path = "/app/config"
+  }
+
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
+  env = [ 
+    "AUTH_API_KEY=${var.firebase_api_key}",
+    "AUTH_DOMAIN=${var.firebase_auth_domain}",
+    "AUTH_PROJECT_ID=${var.firebase_project_id}",
+    "AUTH_STORAGE_BUCKET=${var.firebase_storage_bucket}",
+    "AUTH_MESSAGING_SENDER_ID=${var.firebase_messaging_sender_id}",
+    "AUTH_APP_ID=${var.firebase_app_id}",
+
+    "GOOGLE_APPLICATION_CREDENTIALS=/app/config/project-firebase-adminsdk.json",
+  ]
 }
 
 
