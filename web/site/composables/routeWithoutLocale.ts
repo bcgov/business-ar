19cadeab4d7bb0dc2eb38799b1d0ac9ec5111c1b
deetz99 <dietrich@wolpert.ca>
2024-04-25 17:00:11 -0700

    fix query content composables

--- web/site/composables/routeWithoutLocale.ts
+++ web/site/composables/routeWithoutLocale.ts
@@ -1,23 +1,14 @@
 // return reactive route path without locale prefix
 export function useRouteWithoutLocale (): Ref<string> {
   const route = useRoute()
-  const localeRegex = /^\/[a-zA-Z]{2}-[a-zA-Z]{2}\//
+  const localeRegex = /^\/[a-zA-Z]{2}-[a-zA-Z]{2}(\/|$)/
 
-  // strip the locale prefix from the route path
-  function getRouteWithoutLocale (path: string): string {
-    const match = path.match(localeRegex)
-    if (match) {
-      return path.replace(match[0], '/') // Replace locale prefix with '/'
-    }
-    return path
-  }
-
-  const routeWithoutLocale = ref(getRouteWithoutLocale(route.path))
+  const routeWithoutLocale = ref(route.path.replace(localeRegex, '/'))
 
   watch(
     () => route.path,
     (newPath) => {
-      routeWithoutLocale.value = getRouteWithoutLocale(newPath)
+      routeWithoutLocale.value = newPath.replace(localeRegex, '/')
     }
   )
 
--- web/site/composables/useQueryContentByRoute.ts
+++ web/site/composables/useQueryContentByRoute.ts
@@ -1,6 +1,7 @@
-export const useQueryContentByRoute = async () => {
-  const { locale } = useI18n()
+// return nuxt content file based off current route and locale
+export async function useQueryContentByRoute () {
   const routeWithoutLocale = useRouteWithoutLocale()
+  const { locale } = useI18n()
 
   return await queryContent()
     .where({ _locale: locale.value, _extension: { $eq: 'md' }, _path: { $eq: routeWithoutLocale.value } })
