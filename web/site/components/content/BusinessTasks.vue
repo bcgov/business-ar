d40505bd7baa4c379c23da06b7b13b520fcdd0e9
deetz99 <dietrich@wolpert.ca>
2024-05-28 15:44:45 -0700

    handle business tasks returning empty array

---
+++ web/site/components/content/BusinessTasks.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const busStore = useBusinessStore()
+</script>
+<template>
+  <div v-show="busStore.businessTask === 'initial'">
+    <slot name="initial" />
+  </div>
+  <div v-show="busStore.businessTask === 'none'">
+    <slot name="none" />
+  </div>
+</template>
