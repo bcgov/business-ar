131b780e7187496ece76ad7c123e3ede418f742a
deetz99 <dietrich@wolpert.ca>
2024-06-13 13:45:50 -0700

    fix middleware hangup when tos fails

--- web/site/middleware/02.cleanup.global.ts
+++ web/site/middleware/02.cleanup.global.ts
@@ -1,4 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
+  if (import.meta.server) { return }
+  const pageLoading = useState('page-loading')
   // remove query params in url added by keycloak
   // from https://github.com/bcgov/business-transparency-registry/blob/3a8a8364c3279859e130f2c7aae4feee6b65c5a2/btr-web/btr-common-components/middleware/setupAuth.global.ts#L13
   if (to.query) {
@@ -11,10 +13,13 @@ export default defineNuxtRouteMiddleware((to) => {
   }
 
   // reset pageloading to true betweeen each route ()
-  const pageLoading = useState('page-loading')
-  pageLoading.value = true
+  if (!to.query.fromTos) {
+    pageLoading.value = true
 
-  // reset alerts between pages
-  const alertStore = useAlertStore()
-  alertStore.$reset()
+    // reset alerts between pages
+    const alertStore = useAlertStore()
+    alertStore.$reset()
+  } else {
+    pageLoading.value = false
+  }
 })
