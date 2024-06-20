d3419321b74dda038951707b891951cbfc2e7aee
deetz99 <dietrich@wolpert.ca>
2024-06-20 13:52:13 -0700

    fix help docs not prerendering for modal

--- web/site/components/Sbc/Help/Content.vue
+++ web/site/components/Sbc/Help/Content.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts">
-const { locale } = useI18n()
-
-const { data: helpDocs } = await useAsyncData('help-docs-fetch', () => {
-  return queryContent()
-    .where({ _locale: locale.value, _path: { $eq: '/help' } })
-    .findOne()
-})
+const helpDocs = inject('sbc-bar-help-docs')
 </script>
 <template>
   <ContentRenderer :value="helpDocs" class="prose prose-bcGov prose-h2:mt-4 prose-h2:mb-2 min-w-full break-words text-left leading-snug" />
