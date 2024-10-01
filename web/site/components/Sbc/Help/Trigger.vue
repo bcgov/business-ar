85c2b8697f5df1ec8fab190fb3a29e32cb733ecb
EPortman <Ethan.Portman@gov.bc.ca>
2024-10-01 08:10:33 -0700

    refactor: ui adjustments

--- web/site/components/Sbc/Help/Trigger.vue
+++ web/site/components/Sbc/Help/Trigger.vue
@@ -3,26 +3,24 @@ const helpModal = useHelpModal()
 const localePath = useLocalePath()
 </script>
 <template>
-  <!-- open modal on touch screen, open new tab on desktop -->
-  <!-- hide modal trigger on desktop -->
-  <UButton
-    class="[@media(pointer:fine)]:hidden"
-    variant="outline"
-    :label="$t('btn.openHelpDocs')"
-    icon="i-mdi-help-circle-outline"
-    trailing
-    size="sm"
-    @click="helpModal.open()"
-  />
-  <!-- hide link on touch screen -->
-  <UButton
-    class="[@media(pointer:coarse)]:hidden"
-    variant="outline"
-    :label="$t('btn.openHelpDocs')"
-    icon="i-mdi-help-circle-outline"
-    trailing
-    size="sm"
+  <!-- Open modal on touch screen, open new tab on desktop -->
+  <!-- Hide modal trigger on desktop -->
+  <a
+    class="inline-flex items-center text-blue-600 underline [@media(pointer:fine)]:hidden"
+    href="#"
+    @click.prevent="helpModal.open()"
+  >
+    {{ $t('btn.openHelpDocs') }}
+    <i icon="i-mdi-open-in-new ml-1" />
+  </a>
+
+  <!-- Hide link on touch screen -->
+  <a
+    class="inline-flex items-center text-blue-600 underline [@media(pointer:coarse)]:hidden"
+    :href="localePath('/help')"
     target="_blank"
-    :to="localePath('/help')"
-  />
+  >
+    {{ $t('btn.openHelpDocs') }}
+    <i icon="i-mdi-open-in-new ml-1" />
+  </a>
 </template>
